5b8996fb8d2798b1cc4716b18551a409e19c393f
zacharylutfyr <zacharylutfyr@gmail.com>
2023-06-09 16:51:27 +0700

    Fixing

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 export async function login(email: string, password: string): Promise<AxiosResponse<any>> {
   try {
-    const response = await axios.post('localhost:8080/api/v1/login/save', { email, password });
+    const response = await axios.post('http://localhost:8080/api/v1/login/save', { email, password });
     return response.data;
   } catch (error) {
     throw error;
